c64fed562cf2b10a0fa33e8f2161f1b70faca068
Mert Duzgun <duzgunnmert@gmail.com>
2021-02-07 00:35:43 +0300

    Add overview section to movie component

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,6 +1,12 @@
 const IMG_API = `https://image.tmdb.org/t/p/w500/`;
 
-export const Movie = ({ poster_path, overview, vote_average, children }) => (
+export const Movie = ({
+  poster_path,
+  backdrop_path,
+  overview,
+  vote_average,
+  children,
+}) => (
   <div className="movie">
     <div className="img-wrapper">
       <img src={IMG_API + poster_path} alt={children} />
@@ -15,6 +21,13 @@ export const Movie = ({ poster_path, overview, vote_average, children }) => (
         </div>
         <div className="rate">{vote_average}</div>
       </div>
+    </div>
+    <div
+      className="overview"
+      style={{ backgroundImage: `url(${IMG_API + backdrop_path})` }}
+    >
+      <strong>Overview:</strong>
+      <p>{overview}</p>
     </div>
   </div>
 );
